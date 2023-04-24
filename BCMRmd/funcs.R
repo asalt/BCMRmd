4c831da31a575fb83d12073a117247a392659416
asalt <a.saltzman920@gmail.com>
2023-04-23 22:08:25 -0500

    begin logic for perform gene set analysis

--- BCMRmd/funcs.R
+++ BCMRmd/funcs.R
@@ -25,3 +25,33 @@ recycle_colors <- function(type = "qual", n = 8) {
   recycled_palette <- rep_len(original_palette, length.out = n)
   return(recycled_palette)
 }
+.immune_signature_gids <- c(
+  "920",
+  "925",
+  "3458",
+  "3558",
+  "7124"
+  )
+make_random_gct <- function() {
+  set.seed(369)
+  .mat <- matrix(runif(10000), nrow = 1000, ncol = 10)
+  .rids <- seq(1, dim(.mat)[1]) %>% as.character()
+  .cids <- seq(1, dim(.mat)[2]) %>% as.character()
+  .cdesc <- tibble::tibble(
+    metavar1 = sample(letters[1:5], 10, replace = T),
+    metavar2 = sample(letters[1:5], 10, replace = T),
+  )
+  gct <- cmapR::GCT(mat = .mat, rid = .rids, cid = .cids, cdesc = .cdesc)
+  gct
+}
+
+
+read_rmd_params <- function(.file){
+  library(yaml)
+  header <- readLines(.file)
+  start_index <- which(header == "---")[1]
+  end_index <- which(header == "---")[2] - 1
+  yaml_header <- paste(header[start_index:(end_index)], collapse = "\n")
+  params <- yaml.load(yaml_header, eval.expr = TRUE)$params
+  params
+}
